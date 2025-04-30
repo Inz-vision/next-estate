220179b169326e7a0acbd396bb7dd0c0c1eb02ca
Insy Hernsvan <insy.hernsvan@envisionpharma.com>
2025-04-30 14:27:03 -0400

    Complete the create listing page UI

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "next-estate-5f60a.firebaseapp.com",
+  projectId: "next-estate-5f60a",
+  storageBucket: "next-estate-5f60a.firebasestorage.app",
+  messagingSenderId: "27984604897",
+  appId: "1:27984604897:web:3c2621966822d5c06a11a0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
